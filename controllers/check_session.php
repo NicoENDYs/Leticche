d89bcd280c8e265a781806eeecc767413eb7a310
Nicolas G <guarinmolinan@gmail.com>
2025-04-18 18:19:20 -0500

    En el Admin Check_session.php se desabiito parte del codigo la cual hacia que las sessiones solo duraran 30 minutos

--- controllers/check_session.php
+++ controllers/check_session.php
@@ -6,7 +6,3 @@ if (!isset($_SESSION['usuario_id']) || $_SESSION['cargo'] !== 'ADMIN') {
     header("Location: ../views/login.php?error=acceso_denegado");
     exit();
 }
-
-// En tu check_session.php
-ini_set('session.gc_maxlifetime', 1800); // 30 minutos
-session_set_cookie_params(1800);
