23ef06765774d371192314ed28447047a55e1ca2
Arkzado <jadiazosorio1@gmail.com>
2025-05-06 16:21:43 -0500

    sanitizacion y verificacion del correo

--- controllers/EnviarCorreo.php
+++ controllers/EnviarCorreo.php
@@ -5,8 +5,9 @@ require_once 'Correo.php';
 // Verificamos si el formulario fue enviado
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Recibimos los datos del formulario
-    $correoDestino = filter_var($_POST['correo'], FILTER_SANITIZE_EMAIL);
-
+    $correoDestino = filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL) ? $_POST['correo'] : "El correo '" . $_POST['correo'] . "' es invalido";
+    $correoDestino = filter_var($correoDestino, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    
     // Verificamos que la dirección de correo sea válida
     if (filter_var($correoDestino, FILTER_VALIDATE_EMAIL)) {
         // Instanciamos la clase Correo
@@ -15,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Llamamos al método para manejar la recuperación de contraseña
         $correo->recuperarContrasena($correoDestino);  // Este método se encarga de todo el flujo
     } else {
-        echo "La dirección de correo no es válida.";
+        echo $correoDestino;
     }
 } else {
     echo "No se ha enviado el formulario.";
