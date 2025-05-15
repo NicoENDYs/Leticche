e418588f03287e2601ce4757ffb9b9c252f3cbd5
Arkzado <jadiazosorio1@gmail.com>
2025-05-15 13:12:06 -0500

    cambio en el js productosStorage.js para que aumente la cantidad del carrito respecto al producto y no su cantidad

--- js/productosStorage.js
+++ js/productosStorage.js
@@ -1,27 +1,24 @@
 //obtenemos las llaves de los productos comprados para obtener el total del carrito
 let productosComprados = Object.keys(localStorage);
 let totalCarrito = 0;
-productosComprados.forEach(llaveProducto =>{
-    producto = JSON.parse(localStorage.getItem(llaveProducto));
-    totalCarrito += producto.cantidad;
-})
 
-let contadorCarrito = document.querySelector('.cart-count');
+totalCarrito = productosComprados.length;
+
+let contadorCarrito = document.querySelector(".cart-count");
 contadorCarrito.textContent = totalCarrito;
 
 // obtenemos el producto en formato de objeto
 function almacenarProductoStorage(producto) {
-    
-    //si el producto ya existe en el localStorage, obtenemos su cantidad y la aumentamos 
-    let productoAlmacenado = localStorage.getItem(producto.id)
-    if (productoAlmacenado) {
-        let existente = JSON.parse(productoAlmacenado);
-        producto.cantidad = existente.cantidad + 1;
-    }
+  //si el producto ya existe en el localStorage, obtenemos su cantidad y la aumentamos
+  let productoAlmacenado = localStorage.getItem(producto.id);
+  if (productoAlmacenado) {
+    let existente = JSON.parse(productoAlmacenado);
+    producto.cantidad = existente.cantidad + 1;
+  }
 
-    //guardamos en el localStorage
-    localStorage.setItem(producto.id, JSON.stringify(producto));
+  //guardamos en el localStorage
+  localStorage.setItem(producto.id, JSON.stringify(producto));
 
-    //aumentamos el contador del carrito:
-    contadorCarrito.textContent = parseInt(contadorCarrito.textContent) + 1;
+  //aumentamos el contador del carrito:
+  contadorCarrito.textContent = parseInt(localStorage.length);
 }
