696057daf0b8d7be61039e883f8207b81e6cff66
Arkzado <jadiazosorio1@gmail.com>
2025-05-23 23:02:12 -0500

    arreglo del color opaco del boton de cerrar sesión en los reportes

--- admin/admin_pedidos.php
+++ admin/admin_pedidos.php
@@ -40,7 +40,7 @@ $cantidadPedidosCancelados = ($fila = mysqli_fetch_assoc($traerPedidosCancelados
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Pedidos - Lenticche</title>
+    <title>Gestión de Pedidos - Lenticchie</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
